f9c6cf34a9ba84c783f17a51400e8dbc8c8e1b47
Kakani Aditya Sai <adityasai525@gmail.com>
2019-07-11 11:47:09 +0530

    Added task1

---
+++ Java/Task1.java
@@ -0,0 +1,14 @@
+package task1;
+
+import java.lang.Math;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        double radius=7;
+        double Area=(Math.PI*radius*radius);
+        double Perimeter=(Math.PI*2*radius);
+        System.out.println("Area of the circle is: "+Area);
+        System.out.println("Circumference of the circle is: "+Perimeter);
+    } 
+}
