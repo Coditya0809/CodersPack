a00951eaea3e2c9ae4d77c749ce72d4dc3155392
Kakani Aditya Sai <adityasai525@gmail.com>
2019-07-11 11:48:45 +0530

    Added task 3

---
+++ Java/Task3.java
@@ -0,0 +1,20 @@
+package task3;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int n;
+        int sum_digits=0;
+        System.out.println("Enter any integer: ");
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        
+        for(int i=n;i>0;i=i/10)
+        {
+            sum_digits+=i%10;
+        }
+        System.out.println("The sum of the digits of the given integer is: "+sum_digits);
+    }    
+}
