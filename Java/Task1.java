2adfd9071a9baa4e46893488691f36ccf7419c88
Kakani Aditya Sai <adityasai525@gmail.com>
2019-07-22 12:25:35 +0530

    Made the user to enter the radius of the circle.

--- Java/Task1.java
+++ Java/Task1.java
@@ -5,7 +5,10 @@ import java.lang.Math;
 public class Task1 {
 
     public static void main(String[] args) {
-        double radius=7;
+        double radius;
+        System.out.print("Enter the radius of the circle: ");
+        Scanner s=new Scanner(System.in);
+        radius=s.nextDouble();
         double Area=(Math.PI*radius*radius);
         double Perimeter=(Math.PI*2*radius);
         System.out.println("Area of the circle is: "+Area);
